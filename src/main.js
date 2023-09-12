573c5773e75f6642d8f297bd41bdbc005a600e53
lebohang <lebohang@afrigis.co.za>
2023-09-12 11:55:08 +0200

    import axios  and let vue use it

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import axios from 'axios'; // import axios
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// let's you use axios everywhere throughout the whole project
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 new Vue({
