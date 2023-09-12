7189f3c6f48fb62864ec414816305f30cdd51a31
lebohang <lebohang@afrigis.co.za>
2023-09-12 13:12:49 +0200

    initialized firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import axios from 'axios'; // import axios
+import firebase from 'firebase/app';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,6 +8,19 @@ import store from './store';
 // let's you use axios everywhere throughout the whole project
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC4mFBgFmOmatEJw8YKSNVUBS-6UCftzUM',
+  authDomain: 'qualityproject-8f22a.firebaseapp.com',
+  projectId: 'qualityproject-8f22a',
+  storageBucket: 'qualityproject-8f22a.appspot.com',
+  messagingSenderId: '529768307481',
+  appId: '1:529768307481:web:2dff4c45ce898520a5c388',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
