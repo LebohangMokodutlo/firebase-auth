95b2e7f87bee029eae5b0e89ba4383f1120505a5
lebohang <lebohang@afrigis.co.za>
2023-09-13 09:38:57 +0200

    installed vue/cli and implemented the register form

--- src/views/RegisterForm.vue
+++ src/views/RegisterForm.vue
@@ -1,5 +1,35 @@
 <template>
     <div>
-        <p>Register</p>
+      <div v-if="error" class="error">{{ error.message }}</div>
+      <form @submit.prevent="pressed" >
+        <div class="email">
+            <label for="email">Email:
+            <input type="email" id="email" v-model="email" /></label>
+        </div>
+
+        <div class="password">
+            <label :for="password">Password:
+            <input type="password" id="password" name="password" v-model="password"/></label>
+        </div>
+
+        <button type="submit">Register</button>
+      </form>
     </div>
-</template>
+  </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: '',
+    };
+  },
+  methods: {
+    pressed() {
+      alert('submitted');
+    },
+  },
+};
+</script>
